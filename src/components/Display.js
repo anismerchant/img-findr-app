9a9f528a8cc919faa8515f41eb758c2d79f4bb9e
Anis Merchant <anisxmerchant@gmail.com>
2019-07-25 02:50:54 -0400

    feat: implement drop-down menu to set orientation of images

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import axios from 'axios'
 import { BackTop, Input, Switch } from 'antd'
-import { getPhotos, setPhotoDescFilter, getNotFeatured, getFeatured  } from '../actions'
+import { getPhotos, setPhotoDescFilter, getNotFeatured, getFeatured,
+         getPortrait, getLandscape, getSquarish } from '../actions'
 import { API_ENDPOINT, API_ACCESS_KEY, QUERY_STRING, PARAMETER } from '../api'
 import { findPhoto } from '../selectors'
 import Card from './Card'
 
 class Display extends Component {
   state = {
-    loading: true,
-    orientation: ''
+    loading: true
   }
 
   // Api call
@@ -55,7 +55,31 @@ class Display extends Component {
       this.props.getFeatured()
       this.toggleFeatured()
     }
-  };
+  }
+
+  queryOrientation = (param = '') => {
+    fetch(`${API_ENDPOINT}${QUERY_STRING}${API_ACCESS_KEY}${PARAMETER}&orientation=${param}`)
+    .then(response => {
+      console.log(response)
+      return response.json()
+    })
+    .then(data => {
+      return this.props.getPhotos({ data })
+    })
+  }
+
+  changeOrientation = (e) => {
+    if (e.target.value === "portrait") {
+        this.props.getPortrait()
+        this.queryOrientation(e.target.value)
+    } else if (e.target.value === "landscape") {
+        this.props.getLandscape()
+        this.queryOrientation(e.target.value)
+    } else if (e.target.value === "squarish") {
+        this.props.getSquarish()
+        this.queryOrientation(e.target.value)
+    } 
+  }
 
   render () {
     const { loading } = this.state
@@ -75,6 +99,16 @@ class Display extends Component {
           <Switch id="featured" checked={!loading} onChange={this.onSwitch} />
           <h3>Featured</h3>
         </div>
+        <select
+            value={this.state.orientation}
+            onChange = {this.changeOrientation}
+            className="ant-dropdown-menu"
+          >    
+            <option value="" disabled>Get Images</option>
+            <option value="portrait">Portrait</option>
+            <option value="landscape">Landscape</option>
+            <option value="squarish">Squarish</option>
+        </select>
         <div className="ant-card-container">
           {
             this.displayCard()
@@ -98,7 +132,10 @@ const mapDispatchToProps = (dispatch) => ({
   getPhotos: (payload) => dispatch(getPhotos(payload)),
   setPhotoDescFilter: (payload) => dispatch(setPhotoDescFilter(payload)),
   getNotFeatured: () => dispatch(getNotFeatured()),
-  getFeatured: () => dispatch(getFeatured())
+  getFeatured: () => dispatch(getFeatured()),
+  getPortrait: () => dispatch(getPortrait()),
+  getLandscape: () => dispatch(getLandscape()),
+  getSquarish: () => dispatch(getSquarish()),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Display)
